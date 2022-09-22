61739170bc46abfb94f5e12cbd8a187523f40ce3
Roman Boiko <boykoroman94@gmail.com>
2022-09-22 18:29:21 +0300

    add query validator

--- src/middlewares/queryValidator.js
+++ src/middlewares/queryValidator.js
@@ -1,15 +1,10 @@
-const Joi = require("joi")
-
 const queryValidator = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.query)
     if (!error) {
       next()
     } else {
-      const { details } = error
-      const message = details.map((i) => i.message).join(",")
-      console.log("error", message)
-      res.status(422).json({ error: message })
+      res.status(422).json({ error: error.message })
     }
   }
 }
