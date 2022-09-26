decac8d5e1e23a0ebd8933c3b69dfa82aae10753
Roman Boiko <boykoroman94@gmail.com>
2022-09-26 15:42:55 +0300

    add actors validation

--- src/schemas/movies.js
+++ src/schemas/movies.js
@@ -4,7 +4,7 @@ const postMoviesSchema = Joi.object({
   title: Joi.string().trim().min(1).required(),
   year: Joi.number().min(1850).max(2022).required(),
   format: Joi.string().valid("VHS", "DVD", "Blu-Ray").required(),
-  actors: Joi.array().items(Joi.string()).required(),
+  actors: Joi.array().items(Joi.string().trim().min(1).required()).required(),
 })
 
 module.exports = { postMoviesSchema }
