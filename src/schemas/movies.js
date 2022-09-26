d7b30695517ffeacdedd427f38bb7200e7f951de
Roman Boiko <boykoroman94@gmail.com>
2022-09-26 14:59:22 +0300

    add empty title validation

--- src/schemas/movies.js
+++ src/schemas/movies.js
@@ -1,7 +1,7 @@
 const Joi = require("joi")
 
 const postMoviesSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().trim().min(1).required(),
   year: Joi.number().min(1850).max(2022).required(),
   format: Joi.string().valid("VHS", "DVD", "Blu-Ray").required(),
   actors: Joi.array().items(Joi.string()).required(),
